56e9c613075e6703642d09abbd0ee907ab0fd64c
Rujul <152003018+rujulm@users.noreply.github.com>
2025-07-27 02:26:14 +0400

    Use for loop to create a function that repeats a string

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) { 
+    if (num >= 0) {
+        let final = "";
+        for (let i=0; i<num; i++) {
+            final += string;
+        }
+        return final;
+    }  
+    else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
