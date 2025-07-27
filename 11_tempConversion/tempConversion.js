7af4a8f44181edf2c0c13e93dd05a39f06b583fa
Rujul <152003018+rujulm@users.noreply.github.com>
2025-07-27 19:10:02 +0400

    Create a function to convert temperatures between celsius and fahrenheit

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempC = (temp-32)*(5/9);
+  return Number(tempC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempF = (9/5)*temp+32;
+  return Number(tempF.toFixed(1));
 };
 
 // Do not edit below this line
