27d50e03d4d83eff1b60dc94f5ebbcd71180d7a0
Rujul <152003018+rujulm@users.noreply.github.com>
2025-07-27 18:10:28 +0400

    Create function to return sum of numbers between and including two given numbers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || // parseInt(String(a))!==a
+        !Number.isInteger(b) || // parseInt(String(b))!==b
+        a < 0 || 
+        b < 0) {
+        return 'ERROR';
+    }
+    let sum = 0;
+    let num1 = Math.min(a,b); //a<b ? a : b
+    let num2 = Math.max(a,b); //a<b ? b : a
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    } 
+    return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
